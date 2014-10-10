328023192a7d6c291ad4a43b42b6e497d2749756
Adolfo Hernandez <adolfo.hgarza@gmail.com>
2014-10-10 16:23:26 -0500

    changed stations profile layout css

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
 
   resources :ratings
-
   resources :locations
-
   resources :comments
-
   resources :stations
-
+  resources :users
+  resources :sessions
   get 'paginas/principal'
-
   get "log_out" => "sessions#destroy"
   get "log_in" => "sessions#new"
   get "sign_up" => "users#new"
   root "paginas#principal"
-  resources :users
-  resources :sessions
   
 end

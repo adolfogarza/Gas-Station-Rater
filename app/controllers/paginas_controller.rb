0c0c04daf18929be209b28a0b0188804a39b7cdd
Adolfo Hernandez <adolfo.hgarza@gmail.com>
2014-11-11 20:34:46 -0600

    top stations, recent comments & user panel modules were added

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -1,8 +1,8 @@
 class PaginasController < ApplicationController
-
-
+	
   def principal
   	@user=User.new
-  	@comments = Comment.all.page(params[:page]).per(10)
+  	@recent_comments = Comment.order( 'created_at DESC').all.page(params[:page]).per(10)
+	@top_stations = Station.order('counter_honesty + counter_speed_service + counter_customer_service DESC').limit(5)
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,7 @@
 class Station < ActiveRecord::Base
+	#triggers
+	before_save :asign_defaults
+
 	#associations
 	has_many :comments # define que es el lado muchos de la relacion entre comentarios y estaciones.
 	has_one :location # define que tiene una locacion solamente.
@@ -6,5 +9,13 @@ class Station < ActiveRecord::Base
 
 	#validations
 	validates :legal_name, presence: true, length: { in: 4..35 }, uniqueness: true
-	
+
+	def asign_defaults 
+		if self.counter_honesty.nil?
+			self.counter_honesty = 0
+			self.counter_speed_service = 0
+			self.counter_customer_service = 0
+			self.counter_comments = 0
+		end
+	end
 end
